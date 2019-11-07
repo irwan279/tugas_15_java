69559d7f8a052334b8922040d1c9a0242dbd3395
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 09:02:54 +0700

    tugas 15 java

---
+++ tugas15.js
@@ -0,0 +1,11 @@
+function vitagoras() {
+    var ab = 8;
+    var bc = 6;
+    console.log("Nilai AB",ab);
+    console.log("Nilai BC",bc);
+var hasil = Math.pow(ab,2) + Math.pow(bc,2);
+
+console.log("Maka panjang AC adalah",Math.sqrt(hasil));
+
+}
+vitagoras();
